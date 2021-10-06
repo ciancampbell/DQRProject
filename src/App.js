88c1a1e89438ab7ab305b4df56c7735a90136b23
CIAN CAMPBELL - STUDENT <G00371347@gmit.ie>
2021-10-06 13:22:09 +0100

    Lab2 Week3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Header from './components/header';
+import Contents from './components/contents';
+import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar'; //Need to import the <- for the Nav bar to work
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">  
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/footer">Footer</Nav.Link>
+              <Nav.Link href="/header">Header</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Switch>
+          <Route path = "/" exact ><Contents></Contents></Route>
+          <Route path = "/header"><Header></Header></Route>
+          <Route path = "/footer"><Footer></Footer></Route>
+        </Switch>
+
+      </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
